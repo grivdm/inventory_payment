2dd6aa7090325fb2421067561b7b1e99d1a4b461
Dmitry Grishin <81012708+grivdm@users.noreply.github.com>
2023-12-27 20:57:34 -0500

    Refactor code

--- inventory/app/consumer.py
+++ inventory/app/consumer.py
@@ -1,10 +1,11 @@
 import time
-from app.main import redis
+
+from app.dependencies import redis
 from app.models.product import Product
+
 
 group = "inventory-group"
 key = "order_completed"
-
 
 try:
     redis.xgroup_create(key, group)
@@ -12,14 +13,13 @@ except:
     print("Group already exists")
 
 while True:
-    print("Waiting for messages")
     try:
         results = redis.xreadgroup(group, key, {key: ">"}, None)
         if results:
             for result in results:
                 for message in result[1]:
                     product = Product.get(message[1]["product_id"])
-                    if product:
+                    if product and product.quantity >= int(message[1][b"quantity"]):
                         print(product)
                         product.quantity -= int(message[1][b"quantity"])
                         product.save()
